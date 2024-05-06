3f6232de0a6c784219515bacd9c2c83cac203cb8
ImnalovikaNovella <97136259+ImnalovikaNovella@users.noreply.github.com>
2024-05-06 11:44:43 +0800

    run commit

--- app/Models/Members.php
+++ app/Models/Members.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Members extends Model
 {
     use HasFactory;
+
+    protected $table = 'User';
+
+    protected $fillable =
+    [
+        'name', 'email'
+
+    ];
 }
