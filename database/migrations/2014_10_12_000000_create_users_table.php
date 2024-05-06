2df50086f68e2b6e80397950cd99e84f2e8d0001
ImnalovikaNovella <97136259+ImnalovikaNovella@users.noreply.github.com>
2024-05-06 08:51:52 +0800

    git add filename.txt

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+$host = "localhost";
+$username = "root";
+
+$password ="";
+
+$db = "asetict"; 
+
+
+$connection = new mysqli($host, $username, $password,$db); 
+
+if($connection === false){
+    die ("Not connected to database"); 
+}
+
+//copy the records using the sql query
+
+$sql_query = "INSERT users select * from customers"; 
+
+if($connection->query($sql_query) === true){
+    echo "Data copied successfully"; 
+}
+else{
+    echo"Error occured in copying data"; 
+}
+
+$connection->close();
+
 class CreateUsersTable extends Migration
 {
     /**
